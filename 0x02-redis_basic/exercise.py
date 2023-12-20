057f972dff94d12fabba6889408869986e7228f4
HenryAnyiam <anyiamhenry7@gmail.com>
2023-12-21 00:24:33 +0100

    cache class using redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,59 @@
 from uuid import uuid4
 import redis
 from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """count function calls"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """function call history"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+
+        return output
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """display the history of a particular function"""
+
+    key = method.__qualname__
+    redis_cache = redis.Redis()
+    input_key = key + ":inputs"
+    output_key = key + ":outputs"
+
+    inputs = redis_cache.lrange(input_key, 0, -1)
+    outputs = redis_cache.lrange(output_key, 0, -1)
+
+    calls = redis_cache.get(key).decode('utf-8')
+    print(f"{key} was called {calls} times:")
+
+    for i, o in zip(inputs, outputs):
+        i = i.decode('utf-8')
+        o = o.decode('utf-8')
+
+        print(f"{key}(*{i}) -> {o}")
 
 
 class Cache:
@@ -15,6 +68,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores given data using a generated key"""
         key = str(uuid4())
---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" implement a web cache and tracker"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+_redis = redis.Redis()
+
+
+def count_request(method: Callable) -> Callable:
+    """tracks number of time a URL was accessed in the key"""
+
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """Wrapper function"""
+
+        url = str(*args)
+        _redis.incr(f"count:{url}")
+        cache = _redis.get(f"count:{url}")
+
+        if cache:
+            return cache.decode('utf-8')
+        else:
+            html = method(url)
+            _redis.setex(f"count:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_request
+def get_page(url: str) -> str:
+    """Get HTML content from the URL"""
+
+    result = requests.get(url)
+    return result.text
